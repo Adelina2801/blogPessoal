2f32bf8504b8c2f7fe5bf6153b79860500900c09
Adelina_Santos <adevitoria.m09@gmail.com>
2025-01-29 13:49:32 -0300

    Atualizações finais

--- src/main/java/com/generation/blogpessoal/repository/UsuarioRepository.java
+++ src/main/java/com/generation/blogpessoal/repository/UsuarioRepository.java
@@ -1,13 +1,13 @@
 package com.generation.blogpessoal.repository;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.generation.blogpessoal.model.Usuario;
 
-public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
+import java.util.Optional;
 
-	public Optional<Usuario> findByUsuario(String usuario);
-	
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Optional<Usuario> findByUsuario(String usuario);
 }
